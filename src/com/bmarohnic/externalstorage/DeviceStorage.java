b32c753fbbec23095debcba1c66d0c33a5504582
Brent Marohnic <nednal12@fullsail.edu>
2013-10-08 21:13:35 -0400

    Added DeviceStorage class

---
+++ src/com/bmarohnic/externalstorage/DeviceStorage.java
@@ -0,0 +1,16 @@
+package com.bmarohnic.externalstorage;
+
+import android.content.BroadcastReceiver;
+
+public class DeviceStorage {
+
+	
+	BroadcastReceiver mExternalStorageReceiver;
+	boolean mExternalStorageAvailable = false;
+	boolean mExternalStorageWriteable = false;
+	
+	void updateExternalStorageState()
+	{
+		
+	}
+}
